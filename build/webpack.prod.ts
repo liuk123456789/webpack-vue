80cb5a56692bc42b53b4c0b06b8ed27f08db92f0
kena <18329177975@163.com>
2023-05-05 14:18:24 +0800

    feat: webpack performance

--- build/webpack.prod.ts
+++ build/webpack.prod.ts
@@ -5,11 +5,58 @@ import merge from 'webpack-merge'
 import webpackBaseConfig from './webpack.base'
 
 import CopyPlugin from 'copy-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
+import CompressionPlugin from 'compression-webpack-plugin'
 
 import path from 'path'
 
 const webpackProdConfig: Configuration = merge(webpackBaseConfig, {
   mode: 'production',
+  optimization: {
+    splitChunks: {
+      // 分隔代码
+      cacheGroups: {
+        vendors: {
+          // 提取node_modules代码
+          test: /node_modules/, // 只匹配node_modules里面的模块
+          name: 'vendors', // 提取文件命名为vendors,js后缀和chunkhash会自动加
+          minChunks: 1, // 只要使用一次就提取出来
+          chunks: 'initial', // 只提取初始化就能获取到的模块,不管异步的
+          minSize: 0, // 提取代码体积大于0就提取出来
+          priority: 1 // 提取优先级为1
+        },
+        commons: {
+          // 提取页面公共代码
+          name: 'commons', // 提取文件命名为commons
+          minChunks: 2, // 只要使用两次就提取出来
+          chunks: 'initial', // 只提取初始化就能获取到的模块,不管异步的
+          minSize: 0 // 提取代码体积大于0就提取出来
+        }
+      }
+    },
+    runtimeChunk: {
+      name: 'mainifels'
+    },
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(), // 压缩css
+      new TerserPlugin({
+        parallel: true, // 开启多线程压缩
+        terserOptions: {
+          compress: {
+            pure_funcs: ['console.log'] // 删除console.log
+          }
+        }
+      })
+    ]
+  },
+  performance: {
+    hints: false,
+    maxAssetSize: 4000000, // 整数类型（以字节为单位）
+    maxEntrypointSize: 5000000 // 整数类型（以字节为单位）
+  },
   plugins: [
     new CopyPlugin({
       patterns: [
@@ -19,6 +66,17 @@ const webpackProdConfig: Configuration = merge(webpackBaseConfig, {
           filter: (source) => !source.includes('index.html')
         }
       ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css' // 抽离css的输出目录和名称
+    }),
+    // 打包时生成gzip文件
+    new CompressionPlugin({
+      test: /\.(js|css)$/, // 只生成css,js压缩文件
+      filename: '[path][base].gz', // 文件命名
+      algorithm: 'gzip', // 压缩格式,默认是gzip
+      threshold: 10240, // 只有大小大于该值的资源会被处理。默认值是 10k
+      minRatio: 0.8 // 压缩率,默认值是 0.8
     })
   ]
 })
